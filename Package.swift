0c608bdafc2b485630c4f233af033ed35ad86e7b
southernwind <xmi1996@gmail.com>
2023-11-07 22:22:55 +0900

    ok

--- Package.swift
+++ Package.swift
@@ -5,19 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "opencv2",
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "opencv2",
             targets: ["opencv2"]),
     ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "opencv2"),
-        .testTarget(
-            name: "opencv2Tests",
-            dependencies: ["opencv2"]),
+        .binaryTarget(
+            name: "opencv2",
+            url: "https://github.com/southernwind/opencv-binary/releases/download/v4.8.0/opencv2.xcframework.zip",
+            checksum: "f1e4ae46b0737457ab4511e0bd3d5c4f67be6c9385a0706f6e7924686b26a047")
     ]
 )
